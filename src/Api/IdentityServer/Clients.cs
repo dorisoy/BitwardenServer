731c4638e020b90a0ceb348b252a21a22c07d54e
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-03-17 13:35:24 -0400

    identity client refresh token config

--- src/Api/IdentityServer/Clients.cs
+++ src/Api/IdentityServer/Clients.cs
@@ -9,24 +9,26 @@ namespace Bit.Api.IdentityServer
         {
             return new List<Client>
             {
-                new ApiClient("mobile"),
-                new ApiClient("web"),
-                new ApiClient("browser"),
-                new ApiClient("desktop")
+                new ApiClient("mobile", 60),
+                new ApiClient("web", 1),
+                new ApiClient("browser", 30),
+                new ApiClient("desktop", 30)
             };
         }
 
         public class ApiClient : Client
         {
-            public ApiClient(string id)
+            public ApiClient(string id, int slidingDays)
             {
                 ClientId = id;
                 RequireClientSecret = false;
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword;
                 RefreshTokenExpiration = TokenExpiration.Sliding;
-                SlidingRefreshTokenLifetime = AbsoluteRefreshTokenLifetime;
+                RefreshTokenUsage = TokenUsage.ReUse;
+                SlidingRefreshTokenLifetime = 86400 * slidingDays;
+                AbsoluteRefreshTokenLifetime = 86400 * 365 * 20; // 20 years
                 UpdateAccessTokenClaimsOnRefresh = true;
-                AccessTokenLifetime = 60 * 60; // 1 hour
+                AccessTokenLifetime = 60 * 60 * 1; // 1 hour
                 AllowOfflineAccess = true;
                 AllowedScopes = new string[] { "api" };
             }
